ec1a466841b8d66165fbf7195164dd6e76954b81
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-31 22:11:39 -0300

    feat: add Password class for secure password management with strength evaluation

---
+++ src/core/domain/structures/password.ts
@@ -0,0 +1,42 @@
+export class Password {
+  readonly value: string
+  private static readonly MIN_LENGTH = 8
+
+  private constructor(value: string) {
+    this.value = value
+  }
+
+  static create(value: string): Password {
+    return new Password(value)
+  }
+
+  strength(): number {
+    let score = 0
+    if (this.hasMinLength) score++
+    if (this.hasUppercase) score++
+    if (this.hasLowercase) score++
+    if (this.hasNumber) score++
+    if (this.hasSpecialChar) score++
+    return score
+  }
+
+  get hasMinLength(): boolean {
+    return this.value.length >= Password.MIN_LENGTH
+  }
+
+  get hasUppercase(): boolean {
+    return /[A-Z]/.test(this.value)
+  }
+
+  get hasLowercase(): boolean {
+    return /[a-z]/.test(this.value)
+  }
+
+  get hasNumber(): boolean {
+    return /[0-9]/.test(this.value)
+  }
+
+  get hasSpecialChar(): boolean {
+    return /[!@#$%^&*]/.test(this.value)
+  }
+}
