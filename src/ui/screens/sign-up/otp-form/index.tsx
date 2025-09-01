70603c18a228818dd6dfccfc12b26790993b77d2
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-31 22:13:46 -0300

    feat: implement OtpForm and associated hooks for OTP verification functionality

---
+++ src/ui/screens/sign-up/otp-form/index.tsx
@@ -0,0 +1,24 @@
+import { useClerkAuthService } from '@/ui/hooks/useClerkAuthService'
+import { OtpFormView } from './otp-form-view'
+import { useOtpForm } from './use-otp-form'
+
+type Props = {
+  onConfirm: () => void
+}
+
+export const OtpForm = ({ onConfirm }: Props) => {
+  const { verifyOtpCode } = useClerkAuthService()
+  const { status, isFilled, handleFilled, handleCodeChange } = useOtpForm(
+    verifyOtpCode,
+    onConfirm,
+  )
+
+  return (
+    <OtpFormView
+      status={status}
+      isFilled={isFilled}
+      onChange={handleCodeChange}
+      onFilled={handleFilled}
+    />
+  )
+}
