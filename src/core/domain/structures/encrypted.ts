71fdfca50122c24795694e7c203b10553dfe9d18
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-31 22:11:33 -0300

    feat: implement Encrypted class for secure data handling with decryption functionality

---
+++ src/core/domain/structures/encrypted.ts
@@ -0,0 +1,14 @@
+import type { CryptoProvider } from '@/core/interfaces'
+
+export class Encrypted<Data> {
+  private constructor(readonly value: string) {}
+
+  static create(value: string) {
+    return new Encrypted(value)
+  }
+
+  decrypt(encryptionKey: string, cryptoProvider: CryptoProvider): Data {
+    const decryptedData = cryptoProvider.decrypt(this.value, encryptionKey)
+    return JSON.parse(decryptedData) as Data
+  }
+}
