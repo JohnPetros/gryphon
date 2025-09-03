3f62b028172a4ee14354b9b6c64a87e2f45d2810
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-03 00:07:32 -0300

    feat: create Itens screen component for Vaunt with basic layout

--- src/app/(protected)/vaunts/[vauntId]/itens.tsx
+++ src/app/(protected)/vaunts/[vauntId]/itens.tsx
@@ -1,10 +1,7 @@
 import { Box } from '@/ui/gluestack/box'
 import { Text } from '@/ui/gluestack/text'
-import { useAuthContext } from '@/ui/hooks/use-auth-context'
 
 const Screen = () => {
-  const { account } = useAuthContext()
-  console.log('account', account?.dto)
   return (
     <Box>
       <Text>Vaunt Itens</Text>
