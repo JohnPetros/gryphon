14320f6d36505a8769b547da4b0a40a996f5b9c7
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-15 10:51:10 -0300

    feat: add useClipboard

---
+++ src/ui/hooks/use-clipbaord.ts
@@ -0,0 +1,19 @@
+import * as Clipboard from 'expo-clipboard'
+import { useCallback } from 'react'
+import { useToast } from './use-toast'
+
+export function useClipboard() {
+  const { show } = useToast()
+
+  const copy = useCallback(
+    async (text: string) => {
+      await Clipboard.setStringAsync(text)
+      show('Copiado para a área de transferência', 'success')
+    },
+    [show],
+  )
+
+  return {
+    copy,
+  }
+}
