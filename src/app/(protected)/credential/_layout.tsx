5184f893c8588377ced97a04f59f172fd5bc9fd1
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-18 01:29:18 -0300

    feat: add dynamic credentialId screen to layout for improved navigation

--- src/app/(protected)/credential/_layout.tsx
+++ src/app/(protected)/credential/_layout.tsx
@@ -4,6 +4,7 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name='new' options={{ headerShown: false }} />
+      <Stack.Screen name='[credentialId]' options={{ headerShown: false }} />
     </Stack>
   )
 }
