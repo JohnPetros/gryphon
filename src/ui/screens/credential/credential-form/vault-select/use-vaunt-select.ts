9fe99a00010ff24276e53655f2046886b0fb931c
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-18 01:29:03 -0300

    feat: refactor useVaultSelect and VaultSelectView to manage selected vault state and improve UI interaction

--- src/ui/screens/credential/credential-form/vault-select/use-vaunt-select.ts
+++ src/ui/screens/credential/credential-form/vault-select/use-vaunt-select.ts
@@ -4,13 +4,22 @@ import type { Id } from '@/core/domain/structures'
 import type { VaultsRepository } from '@/core/interfaces'
 import type { Vault } from '@/core/domain/entities'
 
-export const useVaultSelect = (vaultsRepository: VaultsRepository, accountId: Id) => {
+type Params = {
+  vaultsRepository: VaultsRepository
+  accountId: Id
+  onChange: (vaultId: string) => void
+}
+
+export const useVaultSelect = ({ vaultsRepository, accountId, onChange }: Params) => {
   const [vaults, setvaults] = useState<Vault[]>([])
+  const [selectedVault, setSelectedVault] = useState<Vault | null>(null)
 
   useEffect(() => {
     async function loadVaults() {
       const vaults = await vaultsRepository.findAllByAccount(accountId)
       setvaults(vaults)
+      setSelectedVault(vaults[0])
+      onChange(vaults[0].id.value)
     }
 
     loadVaults()
@@ -18,5 +27,6 @@ export const useVaultSelect = (vaultsRepository: VaultsRepository, accountId: Id
 
   return {
     vaults,
+    selectedVault,
   }
 }
--- src/ui/screens/credential/credential-form/vault-select/vault-select-view.tsx
+++ src/ui/screens/credential/credential-form/vault-select/vault-select-view.tsx
@@ -1,5 +1,8 @@
 import type { Vault } from '@/core/domain/entities'
+
 import { Icon } from '@/ui/components/icon'
+import { Pressable } from '@/ui/components/pressable'
+import { Box } from '@/ui/gluestack/box'
 import {
   Select,
   SelectBackdrop,
@@ -10,27 +13,45 @@ import {
   SelectPortal,
   SelectTrigger,
 } from '@/ui/gluestack/select'
-import { SelectInput } from '@/ui/gluestack/select'
+import { Text } from '@/ui/gluestack/text'
 
 type Props = {
   vaults: Vault[]
+  selectedVault: Vault | null
+  onChange: (vaultId: string) => void
 }
 
-export const VaultSelectView = ({ vaults }: Props) => {
+export const VaultSelectView = ({ vaults, selectedVault, onChange }: Props) => {
   return (
     <Select>
-      <SelectTrigger>
-        <SelectInput />
-        <Icon name='arrow-down' />
+      <SelectTrigger className='w-48 h-16 bg-info-background justify-between px-4'>
+        <Text className='text-lg text-accent'>{selectedVault?.title}</Text>
+        <Icon name='arrow-down' color='accent' />
       </SelectTrigger>
       <SelectPortal>
-        <SelectBackdrop />
-        <SelectContent>
+        <SelectBackdrop className='bg-black' />
+        <SelectContent className='px-12 py-4 min-h-64'>
           <SelectDragIndicatorWrapper>
-            <SelectDragIndicator />
+            <SelectDragIndicator className='bg-accent' />
           </SelectDragIndicatorWrapper>
           {vaults.map((vault) => (
-            <SelectItem key={vault.id.value} label={vault.title} value={vault.id.value} />
+            <Pressable
+              key={vault.id.value}
+              onPress={() => onChange(vault.id.value)}
+              className='flex-row items-center justify-between px-6'
+            >
+              <Box className='flex-row items-center'>
+                <Icon name={vault.icon} color='accent' />
+                <SelectItem
+                  key={vault.id.value}
+                  label={vault.title}
+                  value={vault.id.value}
+                  className='-translate-x-2'
+                />
+              </Box>
+
+              <Icon name='check' color='accent' />
+            </Pressable>
           ))}
         </SelectContent>
       </SelectPortal>
