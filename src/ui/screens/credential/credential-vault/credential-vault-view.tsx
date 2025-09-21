40ce45ab954104f7f2fffd86ae16e70f3bbf4234
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-21 14:29:38 -0300

    feat: add CredentialVault component and view for displaying vault information in the credential management interface

---
+++ src/ui/screens/credential/credential-vault/credential-vault-view.tsx
@@ -0,0 +1,18 @@
+import type { Vault } from '@/core/domain/entities'
+
+import { Icon } from '@/ui/components/icon'
+import { Box } from '@/ui/gluestack/box'
+import { Text } from '@/ui/gluestack/text'
+
+type Props = {
+  vault: Vault
+}
+
+export const CredentialVaultView = ({ vault }: Props) => {
+  return (
+    <Box className='flex-row gap-1 px-3 py-3 rounded bg-info-background w-max'>
+      <Icon name={vault.icon} size={16} color='info' />
+      <Text className='text-sm text-info w-max'>{vault.title}</Text>
+    </Box>
+  )
+}
