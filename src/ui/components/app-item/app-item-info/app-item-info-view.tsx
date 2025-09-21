8052b2926ce824764d0e7616a8ba132bcecff831
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-21 14:30:19 -0300

    feat: enhance AppItem components with className merging for improved styling flexibility

--- src/ui/components/app-item/app-item-info/app-item-info-view.tsx
+++ src/ui/components/app-item/app-item-info/app-item-info-view.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@/ui/gluestack/box'
 import { Text } from '@/ui/gluestack/text'
+import { mergeClassNames } from '@/ui/utils'
 
 type Props = {
   name: string
@@ -9,11 +10,11 @@ type Props = {
 
 export const AppItemInfoView = ({ name, description, className }: Props) => {
   return (
-    <Box className={className}>
+    <Box className={mergeClassNames(className)}>
       <Text numberOfLines={1} ellipsizeMode='tail' className='text-md text-accent'>
         {name}
       </Text>
-      <Text isTruncated className='text-sm text-neutral'>
+      <Text numberOfLines={1} ellipsizeMode='tail' className='text-sm text-neutral'>
         {description}
       </Text>
     </Box>
