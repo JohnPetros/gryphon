2954549c638d44d51806d20fa100847b92531d06
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-21 14:31:36 -0300

    feat: add CredentialMenuView and CredentialSettingsScreen components for managing credentials with options to edit, delete, and copy details

---
+++ src/ui/screens/credential-settings/index.tsx
@@ -0,0 +1,32 @@
+import { useLocalSearchParams } from 'expo-router'
+
+import { Id } from '@/core/domain/structures'
+
+import { useDatabase } from '@/ui/hooks/use-database'
+import { CredentialSettingsScreenView } from './credential-settings-screen-view'
+import { useCredentialSettingsScreen } from './use-credential-settings-screen'
+import { useAuthContext } from '@/ui/hooks/use-auth-context'
+
+type LocalSearchParams = {
+  credentialId: string
+}
+
+export const CredentialSettingsScreen = () => {
+  const { credentialId } = useLocalSearchParams<LocalSearchParams>()
+  const { credentialsRepository } = useDatabase()
+  const { encryptionKey } = useAuthContext()
+  const { credential, handleCredentialCreate, handleCredentialUpdate } =
+    useCredentialSettingsScreen(
+      credentialsRepository,
+      credentialId ? Id.create(credentialId) : undefined,
+    )
+
+  return (
+    <CredentialSettingsScreenView
+      credential={credential}
+      encryptionKey={encryptionKey}
+      onCreate={handleCredentialCreate}
+      onUpdate={handleCredentialUpdate}
+    />
+  )
+}
