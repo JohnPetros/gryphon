b9a59f4c3e3d2e8f4cd53c7888d728b61d332c10
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-21 14:31:50 -0300

    feat: add new settings screens for credential management, including routes for creating and viewing credential settings

--- src/app/(protected)/credential/_layout.tsx
+++ src/app/(protected)/credential/_layout.tsx
@@ -3,8 +3,9 @@ import { Stack } from 'expo-router'
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name='new' options={{ headerShown: false }} />
       <Stack.Screen name='[credentialId]' options={{ headerShown: false }} />
+      <Stack.Screen name='settings/new' options={{ headerShown: false }} />
+      <Stack.Screen name='settings/[credentialId]' options={{ headerShown: false }} />
     </Stack>
   )
 }
