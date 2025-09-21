a0ab90310292b950008b33a65b562e70904727f8
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-21 14:46:32 -0300

    refactor: update useClipboard hook to enable clipboard functionality and remove toast notification

--- src/ui/hooks/use-clipbaord.ts
+++ src/ui/hooks/use-clipbaord.ts
@@ -1,17 +1,10 @@
-// import * as Clipboard from 'expo-clipboard'
+import * as Clipboard from 'expo-clipboard'
 import { useCallback } from 'react'
-import { useToast } from './use-toast'
 
 export function useClipboard() {
-  const { show } = useToast()
-
-  const copy = useCallback(
-    async (text: string) => {
-      // await Clipboard.setStringAsync(text)
-      show('Copiado para a área de transferência', 'success')
-    },
-    [show],
-  )
+  const copy = useCallback(async (text: string) => {
+    await Clipboard.setStringAsync(text)
+  }, [])
 
   return {
     copy,
