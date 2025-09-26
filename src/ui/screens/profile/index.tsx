e2e91d95cab34c483df98243c9eec9a510e71d1e
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-26 10:45:38 -0300

    feat: integrate authentication context into profile, displaying account email in the ProfileScreenView component

--- src/ui/screens/profile/index.tsx
+++ src/ui/screens/profile/index.tsx
@@ -1,3 +1,7 @@
+import { useAuthContext } from '@/ui/hooks/use-auth-context'
 import { ProfileScreenView } from './profile-screen-view'
 
-export const ProfileScreen = ProfileScreenView
+export const ProfileScreen = () => {
+  const { account } = useAuthContext()
+  return <ProfileScreenView accountEmail={account?.email ?? ''} />
+}
--- src/ui/screens/profile/profile-screen-view.tsx
+++ src/ui/screens/profile/profile-screen-view.tsx
@@ -6,13 +6,31 @@ import { MinimumPasswordStrenghSelect } from './minimum-password-strengh-select'
 import { Text } from '@/ui/gluestack/text'
 import { MasterPasswordRequirementSwitch } from './master-password-requirement-switch'
 import { AutoLockTimeoutSelect } from './auto-lock-timeout-select'
+import { Input } from '@/ui/components/input'
 
-export const ProfileScreenView = () => {
+
+type Props = {
+  accountEmail: string
+}
+
+export const ProfileScreenView = ({ accountEmail }: Props) => {
   return (
     <ScreenContainer>
       <ScreenTitle>Perfil</ScreenTitle>
 
-      <Box className='mt-12'>
+      <Box className='mt-6'>
+        <Text className='text-lg font-bold text-accent'>Conta</Text>
+        <Box className='mt-4'>
+          <Input
+            label='E-mail'
+            icon='email'
+            defaultValue={accountEmail}
+            isReadOnly
+          />
+        </Box>
+      </Box>
+
+      <Box className='mt-6'>
         <Text className='text-lg font-bold text-accent'>Segurança</Text>
         <Box className='mt-4'>
           <MinimumPasswordStrenghSelect />
