9af32f6bf79485c891c3fe5b474c6eafdbb7c293
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-26 10:48:58 -0300

    chore: update schema version to 15

--- src/database/watermelon/schemas/index.ts
+++ src/database/watermelon/schemas/index.ts
@@ -6,6 +6,6 @@ import { vaultSchema } from './vault-schema'
 import { accountSchema } from './account-schema'
 
 export const schemas = appSchema({
-  version: 12,
+  version: 15,
   tables: [accountSchema, credentialSchema, noteSchema, vaultSchema],
 })
