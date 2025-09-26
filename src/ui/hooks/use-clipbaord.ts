30763893ceaf3479149d8cd4251c2013d3a9e01d
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-25 23:14:47 -0300

    feat: enhance useClipboard hook to show toast notification on copy action

--- src/ui/hooks/use-clipbaord.ts
+++ src/ui/hooks/use-clipbaord.ts
@@ -1,9 +1,13 @@
 import * as Clipboard from 'expo-clipboard'
 import { useCallback } from 'react'
+import { useToast } from './use-toast'
 
 export function useClipboard() {
+  const toast = useToast()
+
   const copy = useCallback(async (text: string) => {
     await Clipboard.setStringAsync(text)
+    toast.show('Copiado para a área de transferência', 'success')
   }, [])
 
   return {
