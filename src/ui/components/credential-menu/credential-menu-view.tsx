92de3dc7a8e43030b7dbb2fe4b629864dad50f44
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-25 23:15:48 -0300

    feat: integrate master password confirmation dialogs for credential copy and edit actions in CredentialMenu

--- src/ui/components/credential-menu/credential-menu-view.tsx
+++ src/ui/components/credential-menu/credential-menu-view.tsx
@@ -1,88 +1,112 @@
+import type { RefObject } from 'react'
+import type { MasterPasswordConfirmationDialogRef } from '../master-password-confirmation-dialog/types'
 import { Link } from 'expo-router'
 
 import type { Id } from '@/core/domain/structures'
 
 import { AppItem } from '../app-item'
 import { Alert } from 'react-native'
+import { MasterPasswordConfirmationDialog } from '../master-password-confirmation-dialog'
 
 type Props = {
   credentialId: Id
   credentialTitle: string
-  onDelete: (credentialId: Id) => void
+  masterPasswordConfirmationForPasswordCopyDialogRef: RefObject<MasterPasswordConfirmationDialogRef | null>
+  masterPasswordConfirmationForCredentialEditionDialogRef: RefObject<MasterPasswordConfirmationDialogRef | null>
   onCopyEmail: () => void
   onCopyPassword: () => void
+  onEdit: () => void
+  onDelete: (credentialId: Id) => void
+  onCorrectMasterPasswordConfirmationForPasswordCopyDialogSubmit: () => void
+  onCorrectMasterPasswordConfirmationForCredentialEditionDialogSubmit: () => void
 }
 
 export const CredentialMenuView = ({
   credentialId,
   credentialTitle,
+  masterPasswordConfirmationForPasswordCopyDialogRef,
+  masterPasswordConfirmationForCredentialEditionDialogRef,
+  onEdit,
   onDelete,
   onCopyEmail,
   onCopyPassword,
+  onCorrectMasterPasswordConfirmationForPasswordCopyDialogSubmit,
+  onCorrectMasterPasswordConfirmationForCredentialEditionDialogSubmit,
 }: Props) => {
   return (
-    <AppItem.Menu>
-      {(close) => (
-        <>
-          <AppItem.MenuOption color='accent' icon='login'>
-            {credentialTitle}
-          </AppItem.MenuOption>
-          <AppItem.MenuOption
-            color='neutral'
-            icon='copy'
-            onPress={() => {
-              onCopyEmail()
-              close()
-            }}
-          >
-            Copiar login
-          </AppItem.MenuOption>
-          <AppItem.MenuOption
-            color='neutral'
-            icon='copy'
-            onPress={() => {
-              onCopyPassword()
-              close()
-            }}
-          >
-            Copiar senha
-          </AppItem.MenuOption>
-          <Link
-            href={`/credential/settings/${credentialId.value}`}
-            onPress={close}
-            asChild
-          >
-            <AppItem.MenuOption color='neutral' icon='edit'>
-              Editar
+    <>
+      <MasterPasswordConfirmationDialog
+        ref={masterPasswordConfirmationForPasswordCopyDialogRef}
+        description='Insira a senha mestra para poder copiar a senha.'
+        canClose
+        onCorrectPasswordSubmit={onCorrectMasterPasswordConfirmationForPasswordCopyDialogSubmit}
+      />
+      <MasterPasswordConfirmationDialog
+        ref={masterPasswordConfirmationForCredentialEditionDialogRef}
+        description='Insira a senha mestra para poder editar a credencial.'
+        canClose
+        onCorrectPasswordSubmit={onCorrectMasterPasswordConfirmationForCredentialEditionDialogSubmit}
+      />
+      <AppItem.Menu>
+        {(close) => (
+          <>
+            <AppItem.MenuOption color='accent' icon='login'>
+              {credentialTitle}
             </AppItem.MenuOption>
-          </Link>
-          <AppItem.MenuOption
-            color='danger'
-            icon='trash'
-            onPress={() => {
-              Alert.alert(
-                'Excluir Credencial?',
-                'Tem certeza que deseja excluir esta credencial? Nunca será possível recuperá-la.',
-                [
-                  {
-                    text: 'Cancelar',
-                    style: 'cancel',
-                  },
-                  {
-                    text: 'Excluir',
-                    onPress: () => {
-                      onDelete(credentialId)
-                      close()
+            <AppItem.MenuOption
+              color='neutral'
+              icon='copy'
+              onPress={() => {
+                onCopyEmail()
+                close()
+              }}
+            >
+              Copiar login
+            </AppItem.MenuOption>
+            <AppItem.MenuOption
+              color='neutral'
+              icon='copy'
+              onPress={() => {
+                onCopyPassword()
+                close()
+              }}
+            >
+              Copiar senha
+            </AppItem.MenuOption>
+              <AppItem.MenuOption color='neutral' icon='edit' onPress={() => {
+                onEdit()
+                close()
+              }}>
+                Editar
+              </AppItem.MenuOption>
+            <AppItem.MenuOption
+              color='danger'
+              icon='trash'
+              onPress={() => {
+                Alert.alert(
+                  'Excluir Credencial?',
+                  'Tem certeza que deseja excluir esta credencial? Nunca será possível recuperá-la.',
+                  [
+                    {
+                      text: 'Cancelar',
+                      style: 'cancel',
                     },
-                  },
-                ],
-              )
-            }}
-          >
-            Excluir
-          </AppItem.MenuOption>
-        </>
-      )}
-    </AppItem.Menu>
+                    {
+                      text: 'Excluir',
+                      onPress: () => {
+                        onDelete(credentialId)
+                        close()
+                      },
+                    },
+                  ],
+                )
+              }}
+            >
+              Excluir
+            </AppItem.MenuOption>
+          </>
+        )}
+      </AppItem.Menu>
+    </>
   )
 }
--- src/ui/components/credential-menu/use-credential-menu.ts
+++ src/ui/components/credential-menu/use-credential-menu.ts
@@ -1,23 +1,32 @@
+import type { RefObject } from 'react'
+
 import type { CredentialsRepository, CryptoProvider } from '@/core/interfaces'
 import type { Id } from '@/core/domain/structures'
 import type { Credential } from '@/core/domain/entities'
 import { useClipboard } from '@/ui/hooks/use-clipbaord'
-import { useRouter } from 'expo-router'
+import type { MasterPasswordConfirmationDialogRef } from '../master-password-confirmation-dialog/types'
+import { NavigationProvider } from '@/core/interfaces/providers/navigation-provider'
 import { ROUTES } from '@/constants'
 
 type Params = {
+  masterPasswordConfirmationForPasswordCopyDialogRef: RefObject<MasterPasswordConfirmationDialogRef | null>
+  masterPasswordConfirmationForCredentialEditionDialogRef: RefObject<MasterPasswordConfirmationDialogRef | null>
   credential: Credential
   encryptionKey: string
   cryptoProvider: CryptoProvider
   repository: CredentialsRepository
+  navigation: NavigationProvider
   onDelete?: () => void
 }
 
 export function useCredentialMenu({
   encryptionKey,
+  masterPasswordConfirmationForPasswordCopyDialogRef,
+  masterPasswordConfirmationForCredentialEditionDialogRef,
   cryptoProvider,
   repository,
   credential,
+  navigation,
   onDelete,
 }: Params) {
   const { copy } = useClipboard()
@@ -31,6 +40,11 @@ export function useCredentialMenu({
     }
   }
 
+  function handleEdit() {
+    navigation.navigate(ROUTES.credentialSettings(credential.id.value))
+    // masterPasswordConfirmationForCredentialEditionDialogRef?.current?.open()
+  }
+
   async function handleCopyEmail() {
     const decryptedData = credential.encrypted.decrypt(encryptionKey, cryptoProvider)
     if (!decryptedData) return
@@ -38,14 +52,27 @@ export function useCredentialMenu({
   }
 
   async function handleCopyPassword() {
+    masterPasswordConfirmationForPasswordCopyDialogRef?.current?.open()
+  }
+
+  async function handleCorrectMasterPasswordConfirmationForPasswordCopyDialogSubmit() {
     const decryptedData = credential.encrypted.decrypt(encryptionKey, cryptoProvider)
     if (!decryptedData) return
     await copy(decryptedData.password)
+    masterPasswordConfirmationForPasswordCopyDialogRef?.current?.close()
+  }
+
+  async function handleCorrectMasterPasswordConfirmationForCredentialEditionDialogSubmit() {
+    navigation.navigate(ROUTES.credentialSettings(credential.id.value))
+    masterPasswordConfirmationForCredentialEditionDialogRef?.current?.close()
   }
 
   return {
     handleCopyEmail,
     handleCopyPassword,
+    handleEdit,
     handleDelete,
+    handleCorrectMasterPasswordConfirmationForPasswordCopyDialogSubmit,
+    handleCorrectMasterPasswordConfirmationForCredentialEditionDialogSubmit,
   }
 }
