2662de725dad64459cd542c9b67cd0abfdd544c9
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-11 15:29:56 -0300

    refactor: remove console log from createRestResponse and add error logging for Axios errors in handleError

--- src/rest/axios/utils/create-rest-response.ts
+++ src/rest/axios/utils/create-rest-response.ts
@@ -4,7 +4,6 @@ import { RestResponse } from '@/core/responses/rest-response'
 export function createRestResponse<ResponseBody>(
   response: AxiosResponse<ResponseBody>,
 ): RestResponse<ResponseBody> {
-  console.log('Axios response', response)
   return new RestResponse<ResponseBody>({
     body: response.data,
     statusCode: response.status,
--- src/rest/axios/utils/handle-error.ts
+++ src/rest/axios/utils/handle-error.ts
@@ -1,6 +1,11 @@
 import { RestResponse } from '@/core/responses/rest-response'
+import { isAxiosError } from 'axios'
 
 export function handleError<ResponseBody>(error: any): RestResponse<ResponseBody> {
+  if (isAxiosError(error)) {
+    console.log('Axios Error', error.message)
+  }
+
   if (error.response) {
     return new RestResponse<ResponseBody>({
       statusCode: error.response.status,
