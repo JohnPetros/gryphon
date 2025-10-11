462c816ff55fdb7ef54779d8dac14550656af42f
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-11 19:41:36 -0300

    feat: enhance ScreenContainer to manage online/offline status and adjust padding dynamically

--- src/ui/components/screen-container/index.tsx
+++ src/ui/components/screen-container/index.tsx
@@ -1,3 +1,10 @@
+import type { PropsWithChildren } from 'react'
+
+import { useInternetContext } from '@/ui/hooks/use-internet-context'
 import { ScreenContainerView } from './screen-container-view'
 
-export const ScreenContainer = ScreenContainerView
+export const ScreenContainer = ({ children }: PropsWithChildren) => {
+  const { isOffline } = useInternetContext()
+
+  return <ScreenContainerView isOffline={isOffline}>{children}</ScreenContainerView>
+}
