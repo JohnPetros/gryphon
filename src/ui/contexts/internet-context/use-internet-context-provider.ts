02dc7c90a2089f0cb9bcea69c1bdc2f413f499e1
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-11 19:42:55 -0300

    fix: update useInternetContextProvider to set isOffline to false, reflecting online status

--- src/ui/contexts/internet-context/use-internet-context-provider.ts
+++ src/ui/contexts/internet-context/use-internet-context-provider.ts
@@ -8,7 +8,7 @@ export function useInternetContextProvider() {
 
   const value: InternetContextValue = useMemo(() => {
     return {
-      isOffline: true,
+      isOffline: false,
     }
   }, [])
 
