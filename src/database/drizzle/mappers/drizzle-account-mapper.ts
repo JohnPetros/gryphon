571a9cc83e5be399b6ba9033b4789fe0ecc61316
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-11 19:26:13 -0300

    feat: implement DrizzleAccountMapper and DrizzleAccountsRepository for account management in the database

---
+++ src/database/drizzle/mappers/drizzle-account-mapper.ts
@@ -0,0 +1,19 @@
+import { Account } from '@/core/domain/entities'
+
+import type { DrizzleAccount } from '../types/drizzle-account'
+
+export const DrizzleAccountMapper = () => {
+  return {
+    toEntity(drizzleAccount: DrizzleAccount): Account {
+      return Account.create({
+        id: drizzleAccount.id,
+        email: drizzleAccount.email ?? '',
+        encryptionSalt: drizzleAccount.encryptionSalt ?? '',
+        isBiometryActivated: drizzleAccount.isBiometryActivated === 1,
+        minimumPasswordStrength: drizzleAccount.minimumPasswordStrength ?? 3,
+        autoLockTimeout: drizzleAccount.autoLockTimeout ?? 0,
+        isMasterPasswordRequired: drizzleAccount.isMasterPasswordRequired === 1,
+      })
+    },
+  }
+}
