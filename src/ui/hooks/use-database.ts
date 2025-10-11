3e25845b6260015cf300e966b081be5ec5f098a4
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-11 13:25:52 -0300

    feat: implement database synchronization logic in useDatabase hook

--- src/ui/hooks/use-database.ts
+++ src/ui/hooks/use-database.ts
@@ -1,4 +1,5 @@
-import { useMemo } from 'react'
+import { useCallback, useEffect, useMemo } from 'react'
+import { synchronize } from '@nozbe/watermelondb/sync'
 
 import {
   WatermelonAccountsRepository,
@@ -6,9 +7,30 @@ import {
   WatermelonCredentialVersionsRepository,
   WatermelonVaultsRepository,
 } from '@/database/watermelon'
+import { watermelon } from '@/database/watermelon/watermelon'
+import { useRest } from './use-rest'
 
 export function useDatabase() {
-  return useMemo(() => {
+  const { databaseService } = useRest()
+
+  const synchronizeDatabase = useCallback(async () => {
+    await synchronize({
+      database: watermelon,
+      pushChanges: async ({ changes, lastPulledAt }) => {},
+      pullChanges: async ({ lastPulledAt }) => {
+        const response = await databaseService.pullDatabaseChanges(
+          lastPulledAt ? new Date(lastPulledAt) : new Date(),
+        )
+        console.log('response', response)
+        return {
+          changes: [],
+          timestamp: Date.now(),
+        }
+      },
+    })
+  }, [])
+
+  const repositories = useMemo(() => {
     return {
       accountsRepository: WatermelonAccountsRepository(),
       credentialsRepository: WatermelonCredentialsRepository(),
@@ -16,4 +38,13 @@ export function useDatabase() {
       credentialVersionsRepository: WatermelonCredentialVersionsRepository(),
     }
   }, [])
+
+  useEffect(() => {
+    synchronizeDatabase()
+  }, [synchronizeDatabase])
+
+  return {
+    ...repositories,
+    synchronizeDatabase,
+  }
 }
