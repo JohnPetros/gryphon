26b3cc91da43981721590886351d6a621561f497
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-31 08:22:30 -0300

    refactor: replace Intl.DateTimeFormat with dayjs for date formatting in useDatetime hook

--- src/ui/hooks/use-datetime.ts
+++ src/ui/hooks/use-datetime.ts
@@ -1,15 +1,13 @@
+import dayjs from 'dayjs'
+import 'dayjs/locale/pt-br'
 import { useCallback } from 'react'
 
 export function useDatetime() {
   const formatDate = useCallback((date: Date) => {
-    const formatted = new Intl.DateTimeFormat('pt-BR', {
-      day: 'numeric',
-      month: 'long',
-      hour: '2-digit',
-      minute: '2-digit',
-    }).format(date)
-
-    return formatted
+    return dayjs(date)
+      .subtract(3, 'hours')
+      .locale('pt-br')
+      .format('D [de] MMMM, HH:mm')
   }, [])
 
   return { formatDate }
