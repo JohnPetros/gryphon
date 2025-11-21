f5a3d9464b141e207593b842bf6e1e633b0b27ad
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-21 17:51:22 -0300

    refactor: simplify layout components by removing NotificationContextProvider and integrating useNotification hook in RootLayout

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -2,17 +2,14 @@ import { Slot } from 'expo-router'
 
 import { AutoLockTimeoutBlockerView } from '@/ui/contexts/autolock-timeout-blocker'
 import { InternetContextProvider } from '@/ui/contexts/internet-context'
-import { NotificationContextProvider } from '@/ui/contexts/notification-context'
 
 const Layout = () => {
   return (
-    <NotificationContextProvider>
-      <AutoLockTimeoutBlockerView>
-        <InternetContextProvider>
-          <Slot />
-        </InternetContextProvider>
-      </AutoLockTimeoutBlockerView>
-    </NotificationContextProvider>
+    <AutoLockTimeoutBlockerView>
+      <InternetContextProvider>
+        <Slot />
+      </InternetContextProvider>
+    </AutoLockTimeoutBlockerView>
   )
 }
 
