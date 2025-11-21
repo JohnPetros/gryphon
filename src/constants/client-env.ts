debf19a15199dd59601e8b0fcdec6a86f8350515
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-21 17:44:07 -0300

    feat: update OneSignal identifiers in client and server environments for consistency

--- src/constants/client-env.ts
+++ src/constants/client-env.ts
@@ -3,13 +3,13 @@ import z from 'zod'
 const clientEnv = {
   gryphonBaseUrl: process.env.EXPO_PUBLIC_GRYPHON_BASE_URL,
   kcvText: process.env.EXPO_PUBLIC_KCV_TEXT,
-  onesignalAppId: process.env.EXPO_PUBLIC_ONESIGNAL_APP_ID,
+  oneSignalAppId: process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID,
 }
 
 const clientEnvSchema = z.object({
   gryphonBaseUrl: z.url(),
   kcvText: z.string().min(1),
-  onesignalAppId: z.string().min(1),
+  oneSignalAppId: z.string().min(1),
 })
 
 export const CLIENT_ENV = clientEnvSchema.parse(clientEnv)
