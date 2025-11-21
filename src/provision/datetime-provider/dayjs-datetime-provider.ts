b6450831cd429792cc3eb2d05d6ab966696b0e36
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-21 17:47:57 -0300

    feat: implement DayjsDateTimeProvider for enhanced date manipulation and formatting

---
+++ src/provision/datetime-provider/dayjs-datetime-provider.ts
@@ -0,0 +1,44 @@
+import dayjs from 'dayjs'
+import 'dayjs/locale/pt-br'
+
+import type { DateTimeProvider } from '@/core/interfaces/providers/datetime-provider'
+
+export const DayjsDateTimeProvider = (): DateTimeProvider => {
+  function getDate(date: Date) {
+    return dayjs(new Date(date.getFullYear(), date.getMonth(), date.getDate()))
+  }
+
+  return {
+    formatDate(date: Date): string {
+      return dayjs(date).subtract(3, 'hours').locale('pt-br').format('D [de] MMMM, HH:mm')
+    },
+
+    addDays(date: Date, days: number): Date {
+      return dayjs(date).add(days, 'days').toDate()
+    },
+
+    addMonths(date: Date, months: number): Date {
+      return dayjs(date).add(months, 'months').toDate()
+    },
+
+    addYears(date: Date, years: number): Date {
+      return dayjs(date).add(years, 'years').toDate()
+    },
+
+    subtractDays(date: Date, days: number): Date {
+      return dayjs(date).subtract(days, 'days').toDate()
+    },
+
+    subtractMonths(date: Date, months: number): Date {
+      return dayjs(date).subtract(months, 'months').toDate()
+    },
+
+    subtractYears(date: Date, years: number): Date {
+      return dayjs(date).subtract(years, 'years').toDate()
+    },
+
+    isAfter(date: Date, otherDate: Date): boolean {
+      return dayjs(getDate(date)).isAfter(getDate(otherDate))
+    },
+  }
+}
---
+++ src/provision/datetime-provider/index.ts
@@ -0,0 +1,1 @@
+export { DayjsDateTimeProvider } from './dayjs-datetime-provider'
