6b7a1ec3a6afc0079fc3f2f69de2a80b5598a7ac
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-21 17:49:31 -0300

    refactor: update credentialVersionSchema to omit updatedAt field for improved schema clarity

--- src/validation/credential-version-schema.ts
+++ src/validation/credential-version-schema.ts
@@ -2,9 +2,11 @@ import { z } from 'zod'
 import { idSchema } from './id-schema'
 import { credentialSchema } from './credential-schema'
 
-export const credentialVersionSchema = credentialSchema.omit({ vaultId: true }).extend({
-  versionNumber: z.number(),
-  isRestoration: z.boolean(),
-  credentialId: idSchema,
-  createdAt: z.coerce.date(),
-})
+export const credentialVersionSchema = credentialSchema
+  .omit({ vaultId: true, updatedAt: true })
+  .extend({
+    versionNumber: z.number(),
+    isRestoration: z.boolean(),
+    credentialId: idSchema,
+    createdAt: z.coerce.date(),
+  })
