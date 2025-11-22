2391bffaf9908e73823c035053e0366be4169813
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-22 10:50:10 -0300

    feat: implement HibpService and NotificationService for password leak checks and notifications

--- src/rest/services/index.ts
+++ src/rest/services/index.ts
@@ -1,2 +1,4 @@
 export { AuthService } from './auth-service'
 export { DatabaseService } from './database-service'
+export { HibpService } from './hibp-service'
+export { NotificationService } from './notification-service'
