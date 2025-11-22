516cb40c2b66b02261224f12b52e4b077cc90f30
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-22 10:49:34 -0300

    feat: implement ExpoCryptoProvider for enhanced cryptographic operations and add hash method to CryptoProvider interface

--- src/core/interfaces/providers/crypto-provider.ts
+++ src/core/interfaces/providers/crypto-provider.ts
@@ -1,6 +1,7 @@
 export interface CryptoProvider {
-  generateSalt: () => Promise<string>
-  deriveKey: (masterPassword: string, salt: string) => Promise<string>
-  encrypt: (data: unknown, encryptionKey: string) => Promise<string>
-  decrypt: (ciphertext: string, encryptionKey: string) => string | null
+  generateSalt(): Promise<string>
+  deriveKey(masterPassword: string, salt: string): Promise<string>
+  encrypt(data: unknown, encryptionKey: string): Promise<string>
+  decrypt(ciphertext: string, encryptionKey: string): string | null
+  hash(data: string): Promise<string>
 }
