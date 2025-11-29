1b3745aa0820d09de6031286b9a76f0b50b67c55
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-29 16:26:30 -0300

    refactor: clean up unused imports in GoogleBackup component and fix text grammar in backup screen

--- src/ui/screens/backup/google-backup-button/index.tsx
+++ src/ui/screens/backup/google-backup-button/index.tsx
@@ -1,15 +1,9 @@
-import { useDatetime } from '@/ui/hooks/use-datetime'
-import { useRest } from '@/ui/hooks/use-rest'
-import { useAuthContext } from '@/ui/hooks/use-auth-context'
 import { useSecureStorage } from '@/ui/hooks/use-secure-storage'
 import { GoogleBackupView } from './google-backup-view'
 import { useGoogleBackup } from './use-google-backup'
 
 export const GoogleBackup = () => {
   const storageProvider = useSecureStorage()
-  const datetimeProvider = useDatetime()
-  const { databaseService } = useRest()
-  const { account } = useAuthContext()
   const {
     accessToken,
     googleAccountEmail,
@@ -19,10 +13,7 @@ export const GoogleBackup = () => {
     handleSignInButtonPress,
     handleSignOutButtonPress,
   } = useGoogleBackup({
-    accountId: account?.id ?? null,
     storageProvider,
-    datetimeProvider,
-    databaseService,
   })
 
   return (
