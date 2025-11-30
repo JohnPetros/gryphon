c706387373b716dcc1a27554e98d9593af2fa820
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-30 12:52:04 -0300

    feat: create ThemeSelectView component for theme selection in profile screen

--- src/ui/screens/profile/theme-select/index.tsx
+++ src/ui/screens/profile/theme-select/index.tsx
@@ -1,5 +1,5 @@
 import { useThemeContext } from '@/ui/hooks/use-theme-context'
-import { ThemeSelectView } from './theme-switch-view'
+import { ThemeSelectView } from './theme-select-view'
 import { useThemeSelect } from './use-theme-select'
 
 export const ThemeSelect = () => {
